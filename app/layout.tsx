4e1821f5eac031073c7800fb6856217eb4a2c098
imshubhamchavan <imshubhamchavan@gmail.com>
2024-06-12 20:36:53 +0530

    adding Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "./components/NextAuthProvider";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {

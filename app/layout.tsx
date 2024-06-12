580bfe1d2cc0882cc3f53df10fe493b976d86257
imshubhamchavan <imshubhamchavan@gmail.com>
2024-06-12 20:18:05 +0530

    adding google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
        <head>
         <link rel="icon" href="/app/favicon.ico" />
+        <meta name="google-site-verification" content="nt8Dh8KJgelsqisLmAV66gQwYzSEwUat2GCwXvJ2MKE" />
       </head>
       <body className={inter.className}>
         <NextAuthProvider>{children}</NextAuthProvider>
